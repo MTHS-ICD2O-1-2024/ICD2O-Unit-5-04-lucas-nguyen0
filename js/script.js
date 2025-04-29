422591df19a52ef13c0e3a47870fe6b9597b33ad
default <lucas.nguyen@mths.ca>
2025-04-29 17:28:09 +0000

    code functions properly

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Lucas Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Nguyen
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+
+
+
+function enterClicked() {
+  const inputAge = document.getElementById('input-age').value
+  const inputDay = document.getElementById('input-day').value
+
+  if (((inputDay == "Tuesday") || (inputDay == "Tuesday")) || ((inputAge > 12) && (inputAge < 21))) {
+    document.getElementById('answer').innerHTML =
+    "You're eligible for student pricing!"
+  } else {
+    document.getElementById("answer").innerHTML =
+    'You must pay regular price.'
+  }
+}
